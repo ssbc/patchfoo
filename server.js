d92635223a233a1bc8d15e6b1d83e50fa74e5e32
cel <cel@f/6sQ6d2CMxRUhLpspgGIulDxDCwYD7DzFzPNr7u5AU=.ed25519>
2017-03-07 20:36:54 -0500

    Pass config as opts

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require('ssb-client')(function (err, sbot, config) {
+var createConfig = require('ssb-config/inject')
+var config = createConfig(process.env.ssb_appname)
+require('ssb-client')(config, function (err, sbot) {
   if (err) throw err
   require('.').init(sbot, config)
 })
