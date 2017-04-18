cc6080bfb402cf088b8a548821a2d1992b637f45
cel <cel@f/6sQ6d2CMxRUhLpspgGIulDxDCwYD7DzFzPNr7u5AU=.ed25519>
2017-04-17 23:39:11 -0700

    Use ssb-party

--- server.js
+++
@@ -1,6 +0,0 @@
-var createConfig = require('ssb-config/inject')
-var config = createConfig(process.env.ssb_appname)
-require('ssb-client')(config, function (err, sbot) {
-  if (err) throw err
-  require('.').init(sbot, config)
-})
